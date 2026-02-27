5e465ca9af6c66f65a02ff8cdc8e0ff47bd7578a
Christian Braimah <chrisbraimah15@gmail.com>
2026-02-26 19:48:19 -0800

    refactor: Update backend deployment configuration and add S3 frontend origin to CORS.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker buildx build --platform linux/amd64 -t chrisbraimah/epiroc-sim:latest --push .
-ssh -i ~/Documents/epiroc-challenge/epiroc-key.pem ubuntu@3.22.224.52 "
-  docker pull chrisbraimah/epiroc-sim:latest
-  docker stop epiroc-sim
-  docker rm epiroc-sim
-  docker run -d --name epiroc-sim --env-file .env.production -p 3000:3000 --restart unless-stopped chrisbraimah/epiroc-sim:latest
+docker buildx build --platform linux/amd64 -t chrisbraimah/epiroc-sim-backend:latest --push .
+ssh -i ~/Documents/epiroc-challenge/epiroc-key.pem ubuntu@3.145.89.180 "
+  sudo docker pull chrisbraimah/epiroc-sim-backend:latest
+  sudo docker stop epiroc-backend
+  sudo docker rm epiroc-backend
+  sudo docker run -d --name epiroc-backend --env-file .env -p 4000:4000 --restart unless-stopped chrisbraimah/epiroc-sim-backend:latest
 "
--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ dotenv.config();
 const allowedOrigins = [
     "http://localhost:5173", 
     "http://localhost:3000",
-    "http://localhost:4000"
+    "http://localhost:4000",
+    "https://epiroc-sim-frontend.s3.us-east-2.amazonaws.com/index.html"
 ];
 
 // CORS configuration
